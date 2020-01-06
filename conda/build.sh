e21c02e8acc2982e82fa62e01042b2f831efe86e
Itay Zandbank <itay@chelem.co.il>
2020-01-06 17:23:42 +0200

    build.sh try one

--- conda/build.sh
+++ conda/build.sh
@@ -1,1 +1,8 @@
-echo No shell building just yet
+#!/bin/bash
+
+cp -R $SRC_DIR $BUILD_PREFIX
+
+cp $SRC_DIR/conda/.env.conda $BUILD_PREFIX/.env
+chmod 666 $BUILD_PREFIX/logstash/sandbox/settings/logstash.yml
+
+mkdir -p $BUILD_PREFIX/mydig-projects/.es/data
