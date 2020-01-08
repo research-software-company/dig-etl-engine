4b6a48ce0823f23b1913bc92cb08eda42c0a28b1
Itay Zandbank <itay@chelem.co.il>
2020-01-08 11:46:18 +0200

    Finish installation in run-engine.sh

--- conda/run-engine.sh
+++ conda/run-engine.sh
@@ -3,4 +3,10 @@
 # This script resides in <conda-env>/bin. We need to run engine.sh from <conda-env>
 
 cd "$(dirname "$0")"/..
+
+# Initialize permissions, as explained in the installation document
+chmod 666 logstash/sandbox/settings/logstash.yml || true
+mkdir -p mydig_projects/.es/data || true
+chown -R 1000:1000 mydig_projects/.es || true
+
 ./engine.sh "$@"
